53a070e183f15ce8af3a900944b69eba00c1458f
kaizey-l <kaylenelimyx@gmail.com>
2026-04-02 21:19:50 +0800

    Update DefendAction.java

--- DefendAction.java
+++ DefendAction.java
@@ -2,8 +2,7 @@ public class DefendAction implements Action {
     public DefendAction() {}
 
     public void execute(Combatant user, Combatant target) {
-    Defendbuff effect = new Defendbuff();
-    user.addEffect(effect);
-    effect.applyEffect(user);
+    DefendBuff effect = new DefendBuff();
+    user.applyStatusEffect(effect);
     }
 }
