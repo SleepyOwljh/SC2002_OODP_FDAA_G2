8ec6489bd13d7b4caf8968e86fbbe82b35880baf
Guan Lin <lowguanlin2004@gmail.com>
2026-04-02 15:44:02 +0800

    added isinvulnerable + isabletoact and its getters and setters, modified removeStatusEffect to only call effect.removeEffect when all duplicate effects are gone

--- Combatant.java
+++ Combatant.java
@@ -8,6 +8,8 @@ public abstract class Combatant {
 	private int defense;
 	private int speed;
 	protected List<StatusEffect> statusEffects;
+	private boolean isInvulnerable;
+	private boolean isAbleToAct;
 
 	public Combatant(int hp, int attack, int defense, int speed) {
 		this.maxHp = hp;
@@ -16,6 +18,8 @@ public abstract class Combatant {
 		this.defense = defense;
 		this.speed = speed;
 		this.statusEffects = new ArrayList<>();
+		this.isInvulnerable = false;
+		this.isAbleToAct = true;
 	}
 
 	public int getHp() {
@@ -65,7 +69,26 @@ public abstract class Combatant {
 		if (amount <= 0) {
 			return;
 		}
+		if (isInvulnerable) {
+			return;
+		}
 		setHp(hp - amount);
+	}
+
+	public boolean getIsInvulnerable() {
+		return isInvulnerable;
+	}
+
+	public void setIsInvulnerable(boolean invulnerable) {
+		this.isInvulnerable = invulnerable;
+	}
+
+	public boolean getIsAbleToAct() {
+		return isAbleToAct;
+	}
+
+	public void setIsAbleToAct(boolean ableToAct) {
+		this.isAbleToAct = ableToAct;
 	}
 
 	public void heal(int amount) {
@@ -89,17 +112,19 @@ public abstract class Combatant {
 	public void removeStatusEffect(StatusEffect effect) {
 		if (effect != null) {
 			statusEffects.remove(effect);
-			effect.removeEffect(this);
-		}
-	}
 
-	public boolean ableToAct() {
-		for (StatusEffect effect : statusEffects) {
-			if (effect instanceof StunEffect) {
-				return false;
+			boolean sameTypeStillExists = false;
+			for (StatusEffect currentEffect : statusEffects) {
+				if (currentEffect.getClass() == effect.getClass()) {
+					sameTypeStillExists = true;
+					break;
+				}
+			}
+
+			if (!sameTypeStillExists) {
+				effect.removeEffect(this);
 			}
 		}
-		return true;
 	}
 
 	public void processTurnStart() {
