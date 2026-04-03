4d0c9afb275a9f29ec01adb350058134efcfc2b5
lightning259 <163219855+lightning259@users.noreply.github.com>
2026-04-03 11:11:51 +0800

    Update Potion.java

--- Potion.java
+++ Potion.java
@@ -4,14 +4,5 @@ public class Potion implements Item{
     public Potion(){
         this.healAmount = 100;
     }
-    public void useItem(Combatant user, Combatant target){
-        int hp = target.getHp();
-        int maxHp = target.getMaxHp();
-        int newHp = Math.min(hp + healAmount, maxHp);
-        target.setHp(newHp);
-        System.out.println("You used the potion and received healing!");
-        System.out.println("Current HP: " + newHp);
-
-    }
 }
 
