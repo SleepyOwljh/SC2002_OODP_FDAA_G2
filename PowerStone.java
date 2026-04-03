699af55959989753ee870553222022c0aa378ddf
lightning259 <163219855+lightning259@users.noreply.github.com>
2026-04-03 11:14:51 +0800

    Update PowerStone.java

--- PowerStone.java
+++ PowerStone.java
@@ -1,4 +1,5 @@
 public class PowerStone implements Item{
+    public PowerStone(){}
     public void useItem(Combatant user, Combatant target){
         user.getSpecialSkillAction();
         user.execute();
