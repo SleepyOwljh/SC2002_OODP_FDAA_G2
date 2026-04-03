61eabf5918999c7aee045cfb0ede78875c7b4930
lightning259 <163219855+lightning259@users.noreply.github.com>
2026-04-03 11:13:14 +0800

    Update SmokeBomb.java

--- SmokeBomb.java
+++ SmokeBomb.java
@@ -4,7 +4,6 @@ public class SmokeBomb implements Item{
         SmokeBombEffect effect = new SmokeBombEffect();
         user.applyStatusEffect(effect);
         effect.applyEffect(user);
-        System.out.println("You used the SmokeBomb! The enemies cannot hurt you for this turn and the next!");
     }
 
 }
