c56237a8db831a0bc82c4975461d4f485e852c38
Liew Jun Hao <LIEW0241@e.ntu.edu.sg>
2026-04-08 15:25:45 +0800

    remove 'active' from defend buff

--- DefendBuff.java
+++ DefendBuff.java
@@ -1,24 +1,16 @@
 public class DefendBuff extends TemporaryEffects {
     private int defenseBoost;
-    private boolean active;
 
     public DefendBuff() {
         super(2);
         this.defenseBoost = 10;
-        this.active = false;
     }
 
     public void applyEffect(Combatant target) {
-        if (this.active) {
-            extendDuration(1);
-        } else {
-            target.setDefense(target.getDefense() + defenseBoost);
-            this.active = true;
-        }
+        target.setDefense(target.getDefense() + defenseBoost);
     }
 
     public void removeEffect(Combatant target) {
         target.setDefense(target.getDefense() - defenseBoost);
-        this.active = false;
     }
 }
