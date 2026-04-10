fd29eb625bb807fa71a82c3ddaf7877e08356621
Guan Lin <lowguanlin2004@gmail.com>
2026-04-10 21:24:28 +0800

    added short documentation for constructor methods in combatant's grandchild classes

--- Wizard.java
+++ Wizard.java
@@ -3,6 +3,7 @@ public class Wizard extends Player {
 	private final Action specialSkill;
 
 	public Wizard() {
+		// Health: 200, Attack: 50, Defense: 10, Speed: 20
 		super(200, 50, 10, 20);
 		this.specialSkill = new ArcaneBlastAction();
 	}
--- Wolf.java
+++ Wolf.java
@@ -1,6 +1,7 @@
 public class Wolf extends Enemy {
 
 	public Wolf() {
+		// Health: 40, Attack: 45, Defense: 5, Speed: 35
 		super(40, 45, 5, 35);
 	}
 }
